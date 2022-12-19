2aeefefb316611bd69fca3b6b2290a62a8a7491c
Brendan Speer <brendan.speer@gmail.com>
2022-12-19 09:08:55 -0600

    * Changes for Refresh WOrking.

--- src/cljs/ti4_web_frontend/core.cljs
+++ src/cljs/ti4_web_frontend/core.cljs
@@ -1,15 +1,13 @@
 (ns ti4-web-frontend.core
-  (:require
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]
-   [reagent.session :as session]
-   [reitit.frontend :as reitit]
-   [clerk.core :as clerk]
-   [accountant.core :as accountant]
-   [ajax.core :refer [GET]]
-   [cljs.core.async :as a :refer [go, go-loop, <!]]
-   [haslett.client :as ws]
-   [haslett.format :as fmt]))
+  (:require [accountant.core :as accountant]
+            [ajax.core :refer [GET]]
+            [clerk.core :as clerk]
+            [cljs.core.async :as a :refer [<! >! alts! go-loop timeout go put!]]
+            [haslett.client :as ws]
+            [reagent.core :as reagent :refer [atom]]
+            [reagent.dom :as rdom]
+            [reagent.session :as session]
+            [reitit.frontend :as reitit]))
 
 ;; -------------------------
 ;; Routes
@@ -94,40 +92,52 @@
                      :response-format :json
                      :keywords? true}) 
   (let [data (atom nil)
-        imgurl (atom "http://ti4.westaddisonheavyindustries.com/map/pbd254/2022-12-18T04%3A23%3A45.244735447.png")]
+        imgurl (atom "")
+        sstream (atom nil)]
 
 
     (reagent/create-class {:display-name "my-comp"
 
                            :component-did-mount
                            (fn []
-                             (let [ti4ws (ws/connect (str "wss://4z4c1wj2e2.execute-api.us-east-1.amazonaws.com/dev?map=" (get-in (session/get :route) [:route-params :game-id])))]
-                               (go (let [stream (<! ti4ws)]
-                                     (go-loop [] (let [newmap (<! (:source stream))]
-                                                   (when newmap 
-                                                     (println newmap)
-                                                     (reset! imgurl newmap))
-                                                   (recur))))))
-                             (GET (str "https://bbg9uiqewd.execute-api.us-east-1.amazonaws.com/Prod/map/" (get-in (session/get :route) [:route-params :game-id]))
-                               {:handler         (fn [x] (reset! data x))
-                                :response-format :json
-                                :keywords?       true})
+                             (go (let [stream (<! (ws/connect (str "wss://4z4c1wj2e2.execute-api.us-east-1.amazonaws.com/dev?map=" 
+                                                                   (get-in (session/get :route) [:route-params :game-id]))))]
+                                   (reset! sstream stream)
+                                   (>! (:sink stream) (str "MAP " (get-in (session/get :route) [:route-params :game-id])))
+                                (go-loop [stream stream] (let [[val ch] (alts! [(timeout 300000) (:source stream)])]
+                                              (if (= ch (:source stream))
+                                                (when (and (not (= val "PONG"))  (not (nil? val))) 
+                                                  (reset! imgurl val))
+                                                (>! (:sink stream) "PING"))
+                                              (when-not (and (= ch (:source stream)) (nil? val))
+                                                (recur stream))
+                                              (when-not (= ch (:source stream))
+                                                (recur stream))
+                                              ))))
+                             
+
+                             
                              (let [routing-data (session/get :route)
                                    item-name (get-in routing-data [:route-params :game-id])
                                    item (first (filter #(= item-name (:MapName %)) (:games @state)))]
-                               (reset! imgurl (:MapURL item))
-                               (set! (.-title js/document) (if @data (str (:MapName item) " - Round " (:round @data)) (:MapName item)))))
-
-                           ;:component-did-update (fn [this]
-                           ;  (let [new-notes (rest (reagent/argv this))]
-                           ;    (apply draw-system-with-chord new-notes)))
+                               (reset! imgurl (:MapURL item)) 
+                               (GET (str "https://bbg9uiqewd.execute-api.us-east-1.amazonaws.com/Prod/map/" (get-in (session/get :route) [:route-params :game-id])) 
+                                 {:handler         (fn [x] (reset! data x) 
+                                                     (set! (.-title js/document) (if @data (str (:MapName item) " - Round " (:round @data)) (:MapName item)))) 
+                                  :response-format :json 
+                                  :keywords?       true})
+                               ))
+                           
+                           :component-will-unmount
+                           (fn []
+                             (ws/close @sstream))
                            
                            :reagent-render
                            (fn []
                              (let [routing-data (session/get :route)
                                    item-name (get-in routing-data [:route-params :game-id])]
                                [:span.main
-                                [:h1 (str "Game " item-name "")]
+                                [:h1 (str "Game " item-name "")][:input {:type "button" :value "Load Map" :on-click #(put! (:sink @sstream) (str "MAP " item-name))}]
                                 [:img {:src @imgurl}]
                                 [:p [:a {:href (path-for :index)} "Back to the list of games"]]]))})))
 
